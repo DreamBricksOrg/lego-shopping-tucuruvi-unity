90154697c3ac29b3d0326fb021ff2334eea2e9cf
Julio Balbino <julio4110@hotmail.com>
2025-11-12 18:32:19 -0300

    feat(instructions): add pulsing circle button effect

--- Assets/Scripts/INSTRUCOES.cs
+++ Assets/Scripts/INSTRUCOES.cs
@@ -13,6 +13,12 @@ public class INSTRUCOES : MonoBehaviour
     public float totalTime;
     public float currentTime;
 
+    public RawImage circleButton;
+    public float pulseSpeed = 2f;
+    public float minAlpha = 0.4f;
+    public float maxAlpha = 1f;
+    private Color circleInitialColor;
+
     private void Awake()
     {
         config = new();
@@ -22,6 +28,11 @@ public class INSTRUCOES : MonoBehaviour
     {
         currentTime = totalTime;
         timerRunning = true;
+
+        if (circleButton != null)
+        {
+            circleInitialColor = circleButton.color;
+        }
     }
 
     private void Update()
@@ -35,6 +46,23 @@ public class INSTRUCOES : MonoBehaviour
                 timerRunning = false;
                 SceneManager.LoadScene("SampleScene");
             }
+        }
+
+        if (circleButton != null)
+        {
+            float t = Mathf.PingPong(Time.time * pulseSpeed, 1f);
+            float a = Mathf.Lerp(minAlpha, maxAlpha, t);
+            var c = circleButton.color;
+            c.a = a;
+            circleButton.color = c;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (circleButton != null)
+        {
+            circleButton.color = circleInitialColor;
         }
     }
 
